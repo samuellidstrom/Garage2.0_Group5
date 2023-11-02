0e1cfe84505899a13dff8c6788348a0f9c4ccb01
Samuel Lidström <samuel.lidstrom@gmail.com>
2023-11-02 11:49:12 +0100

    Add project files.

---
+++ Garage2.0_Group5/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Garage2._0_Group5.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
