172624081bddb68c3ffeea40f019914717b026d4
Samuel Lidström <samuel.lidstrom@gmail.com>
2023-11-02 15:11:31 +0100

    Added CreateVehicle View Vehicle-class and navigation to the view

--- Garage2.0_Group5/Controllers/HomeController.cs
+++ Garage2.0_Group5/Controllers/HomeController.cs
@@ -23,10 +23,10 @@ namespace Garage2._0_Group5.Controllers
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        public IActionResult ParkVehicle(Vehicle vehicle)
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View("ParkVehicle");
         }
+
     }
 }
---
+++ Garage2.0_Group5/Models/Vehicle.cs
@@ -0,0 +1,13 @@
+﻿namespace Garage2._0_Group5.Models
+{
+    public class Vehicle
+    {
+        public string Type { get; set; }
+        public string LicenseNumber { get; set; }
+        public string Color { get; set; }
+        public string Brand{ get; set; }
+        public string Model { get; set; }
+        public string NoOfWheels { get; set; }
+        public DateTime? TimeOfRegistration { get; set; }
+    }
+}
