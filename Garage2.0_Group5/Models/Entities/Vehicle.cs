9daaa5fb3526d65f2f0823ce9387b7395754a45c
Farahalh <120371923+Farahalh@users.noreply.github.com>
2023-11-07 12:00:50 +0100

    Adding a regular expression for license number format, preprocessing to eliminate whitespace and connverting input to upper case.
    next step, make sure same registration number does not exist on multiple vehicles at a time.

--- Garage2.0_Group5/Models/Entities/Vehicle.cs
+++ Garage2.0_Group5/Models/Entities/Vehicle.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
 
 namespace Garage2._0_Group5.Models.Entities
 {
@@ -8,8 +9,21 @@ namespace Garage2._0_Group5.Models.Entities
         public int Id { get; set; }
         public TypeOfVehicle Type { get; set; }
 
+
+        private string licenseNumber;
+
         [DisplayName("License Number")]
-        public string LicenseNumber { get; set; }
+        [RegularExpression(@"^[a-zA-Z]{3}\d{3}$", ErrorMessage = "Registration format must be ABC123")]
+        [StringLength(6, MinimumLength = 6)]
+        public string LicenseNumber { get 
+            {
+                return licenseNumber;
+            }
+            set
+            {
+                licenseNumber = Regex.Replace(value, @"\s", "").ToUpper();
+            }
+        }
 
         public Color Color { get; set; }
 
