5f4e640d0bb56cf37cee357dc12a8d2ce3f9f85a
Samuel Lidström <samuel.lidstrom@gmail.com>
2023-11-09 01:06:48 +0100

    Everything works

--- Garage2.0_Group5/Migrations/20231109000151_Init.Designer.cs
+++ Garage2.0_Group5/Migrations/20231109000151_Init.Designer.cs
@@ -12,7 +12,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Garage2._0_Group5.Migrations
 {
     [DbContext(typeof(Garage2_0_Group5Context))]
-    [Migration("20231106212416_Init")]
+    [Migration("20231109000151_Init")]
     partial class Init
     {
         /// <inheritdoc />
@@ -20,18 +20,16 @@ namespace Garage2._0_Group5.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "7.0.12")
+                .HasAnnotation("ProductVersion", "7.0.13")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128);
 
             SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder);
 
             modelBuilder.Entity("Garage2._0_Group5.Models.Entities.Vehicle", b =>
                 {
-                    b.Property<int>("Id")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("int");
-
-                    SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("Id"));
+                    b.Property<string>("ID")
+                        .HasMaxLength(6)
+                        .HasColumnType("nvarchar(6)");
 
                     b.Property<string>("Brand")
                         .IsRequired()
@@ -40,10 +38,6 @@ namespace Garage2._0_Group5.Migrations
 
                     b.Property<int>("Color")
                         .HasColumnType("int");
-
-                    b.Property<string>("LicenseNumber")
-                        .IsRequired()
-                        .HasColumnType("nvarchar(max)");
 
                     b.Property<string>("Model")
                         .IsRequired()
@@ -59,7 +53,7 @@ namespace Garage2._0_Group5.Migrations
                     b.Property<int>("Type")
                         .HasColumnType("int");
 
-                    b.HasKey("Id");
+                    b.HasKey("ID");
 
                     b.ToTable("Vehicle");
                 });
