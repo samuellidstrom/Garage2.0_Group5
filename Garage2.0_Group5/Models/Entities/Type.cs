e06ba37704e63daf2e5a46ad5f2bfff5639635a1
Samuel Lidström <samuel.lidstrom@gmail.com>
2023-11-20 10:22:55 +0100

    Started working on my first task

---
+++ Garage2.0_Group5/Models/Entities/Type.cs
@@ -0,0 +1,9 @@
+﻿namespace Garage2._0_Group5.Models.Entities
+{
+    public class Type
+    {
+        public int Id { get; set; }
+        public string VehicleType { get; set; }
+        public int Wheels { get; set; }
+    }
+}
