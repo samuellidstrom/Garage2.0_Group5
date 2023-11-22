0ecf007c95df60f526e2b4f08e379df725080627
Samuel Lidström <samuel.lidstrom@gmail.com>
2023-11-22 10:01:16 +0100

    Final fixes before merge to main

--- Garage2.0_Group5/Models/Entities/VehicleType.cs
+++ Garage2.0_Group5/Models/Entities/VehicleType.cs
@@ -1,12 +1,22 @@
-﻿namespace Garage2._0_Group5.Models.Entities
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace Garage2._0_Group5.Models.Entities
 {
     #nullable disable
     public class VehicleType
     {
         public int Id { get; set; }
-        public string TypeOfVehicle { get; set; }
-        public int Wheels { get; set; }
 
+        [Required]
+        [DisplayName("Vehicle Type")]
+        [StringLength(20, MinimumLength = 3)]
+        public string TypeOfVehicle { get; set; }
+
+        [Required]
+        [DisplayName("Number of Wheels")]
+        [Range(0, 18)]
+        public int Wheels { get; set; }
 
         //Foreign key
         public int VehicleId { get; set; }
--- Garage2.0_Group5/Models/ViewModels/VehicleEditViewModel.cs
+++ Garage2.0_Group5/Models/ViewModels/VehicleEditViewModel.cs
@@ -3,14 +3,15 @@
 namespace Garage2._0_Group5.Models.ViewModels
 {
 #nullable disable
-    public class VehicleCreateViewModel
+    public class VehicleEditViewModel
     {
+        public int Id { get; }
         public string LicenseNumber { get; set; }
         public Color Color { get; set; }
         public string Brand { get; set; }
         public string Model { get; set; }
         public string TypeOfVehicle { get; set; }
         public int Wheels { get; set; }
-        public DateTime TimeOfRegistration { get; set; } = DateTime.Now;
+        public DateTime TimeOfRegistration { get; }
     }
 }
